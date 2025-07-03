9272096ef373a45adee838160f410fac6efa5980
vansh-home <dadhichvansh46@gmail.com>
2025-07-03 20:50:34 +0530

    disable react prop types checking

--- eslint.config.js
+++ eslint.config.js
@@ -24,6 +24,7 @@ export default [
       ...js.configs.recommended.rules,
       ...reactHooks.configs.recommended.rules,
       'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+      'react/propt-types': 'off',
       'react-refresh/only-export-components': [
         'warn',
         { allowConstantExport: true },
