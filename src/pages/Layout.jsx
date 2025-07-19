8b8ead62460edaf951427968bf56e61da4b8ceb2
vansh-home <dadhichvansh46@gmail.com>
2025-07-19 20:14:10 +0530

    update layout

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -1,9 +1,20 @@
-import { Outlet, useNavigation } from "react-router-dom";
+import { useNavigation } from "react-router-dom";
+import { useEffect } from "react";
 import Loader from "../components/ui/Loader";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import Hero from "../components/Hero";
+import About from "../components/About";
+import Contact from "../components/Contact";
+import MenuHighlights from "../components/MenuHighlights";
+import LocationHours from "../components/LocationHours";
+import ScrollToTopButton from "../components/ScrollToTopButton";
 
 const Layout = () => {
+  useEffect(() => {
+    document.title = "Brew & Blend Café | Artisanal Coffee & Cozy Vibes";
+  }, []);
+
   const navigation = useNavigation();
 
   if (navigation.state === "loading") return <Loader />;
@@ -11,9 +22,14 @@ const Layout = () => {
     <>
       <Header />
       <main>
-        <Outlet />
+        <Hero />
+        <MenuHighlights />
+        <About />
+        <LocationHours />
+        <Contact />
       </main>
       <Footer />
+      <ScrollToTopButton />
     </>
   );
 };
