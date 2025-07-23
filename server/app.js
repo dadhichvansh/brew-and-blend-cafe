e5dbe262bcabeee88099006d781e6a3c71bd1744
vansh-home <dadhichvansh46@gmail.com>
2025-07-23 10:56:47 +0530

    connected database and define routes

--- server/app.js
+++ server/app.js
@@ -1,16 +1,29 @@
 import "dotenv/config";
 import express from "express";
 import cors from "cors";
+import path from "path";
+import { connectDB } from "./src/db/db.js";
 import { PORT } from "./src/validations/port.validation.js";
+import { menuRoute } from "./src/routes/menu/menu.route.js";
 
 const app = express();
+const staticPath = path.join(import.meta.dirname, "public", "images");
+connectDB();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(staticPath));
+app.use("/uploads", express.static("uploads"));
 app.use(
   cors({
     origin: ["http://localhost:5173"],
     methods: ["GET"],
   })
 );
+
+// routes
+app.use("/", menuRoute);
+app.use("/admin", menuRoute);
 
 app.listen(PORT, () =>
   console.log(`Listening to server on http://localhost:${PORT}`)
