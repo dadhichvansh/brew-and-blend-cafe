ce06d5c17a1adf96538304185981de5300c96761
vansh-home <dadhichvansh46@gmail.com>
2025-08-27 00:08:32 +0530

    rename to menu-item.model.js

---
+++ server/src/models/menu-item.model.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+const menuItemSchema = new mongoose.Schema(
+  {
+    image: { type: String, required: false },
+    title: { type: String, required: true, unique: true },
+    price: { type: Number, required: true },
+    description: { type: String, required: true },
+    category: { type: String, required: true },
+  },
+  { timestamps: true }
+);
+
+export const MenuItem = mongoose.model("MenuItem", menuItemSchema);
